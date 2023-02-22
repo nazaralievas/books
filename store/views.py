c66589cc4fe20532aca96d78b3a3c3ac20db6226
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-02-23 01:28:32 +0600

    filters added

--- store/views.py
+++ store/views.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+from rest_framework.filters import OrderingFilter
 
 from .models import Book
 from .serializers import BooksSerializer
@@ -7,3 +10,7 @@ from .serializers import BooksSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['price']
+    search_fields = ['name', 'author_name']
+    ordering_fields = ['price', 'author_name']
