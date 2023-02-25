e7bcf9b6164134f8e072b430b6be822dd4c01186
Saadat366 <62797589+Saadat366@users.noreply.github.com>
2023-02-25 18:40:47 +0600

    owner attribute added to model Book; permissions for owner added; tests for permission only fuctionality

--- store/views.py
+++ store/views.py
@@ -2,21 +2,25 @@ from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django.shortcuts import render
 
 from .models import Book
 from .serializers import BooksSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class BookViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly]
     queryset = Book.objects.all()
+    permission_classes = [IsOwnerOrReadOnly]
     serializer_class = BooksSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
 
 def auth(request):
